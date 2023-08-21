806e45eeca4296008630d00b32461073bbffd1f3
Rashad Novruzlu <rashad_ny@bestcomp.net>
2023-08-22 01:00:09 +0400

    Add project files.

---
+++ FileFolderRenamer/FileCommand.cs
@@ -0,0 +1,96 @@
+﻿using System.IO.Compression;
+using System.Reflection;
+
+namespace FileFolderRenamer
+{
+    public sealed class FileCommand
+    {
+        public FileCommand(string currentProjectName, string newProjectName, string newProjectPath)
+        {
+            CurrentProjectName = currentProjectName;
+            NewProjectName = newProjectName;
+            NewProjectPath = newProjectPath;
+        }
+
+        public string NewProjectPath { get; set; }
+        public string CurrentProjectName { get; set; }
+        public string NewProjectName { get; set; }
+         
+        public void ReplaceFolderName(string dir)
+        {
+            try
+            {
+
+                foreach (string folder in Directory.GetDirectories(dir))
+                {
+                    string folderName = folder;
+
+                    if (folder.IndexOf(CurrentProjectName) != -1)
+                    {
+                        folderName = folder.Replace(CurrentProjectName, NewProjectName);
+
+                        Directory.Move(folder, folderName);
+                    }
+
+                    ReplaceFolderName(folderName);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void ReplaceFileName(string dir)
+        {
+            try
+            {
+                foreach (string file in Directory.GetFiles(dir))
+                {
+                    if (file.IndexOf(CurrentProjectName) != -1)
+                    {
+                        File.Move(file, file.Replace(CurrentProjectName, NewProjectName));
+                    }
+                }
+
+                foreach (string folder in Directory.GetDirectories(dir))
+                {
+                    ReplaceFileName(folder);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void ReplaceFileContent(string dir)
+        {
+            try
+            {
+                foreach (string file in Directory.GetFiles(dir))
+                {
+
+                    string text = File.ReadAllText(file);
+                    text = text.Replace(CurrentProjectName, NewProjectName);
+                    File.WriteAllText(file, text);
+
+                }
+
+                foreach (string folder in Directory.GetDirectories(dir))
+                {
+                    ReplaceFileContent(folder);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
+
---
+++ FileFolderRenamer/Program.cs
@@ -0,0 +1,26 @@
+﻿using FileFolderRenamer;
+
+string currenstolutionName = "e-commerce";
+
+string newSolutionName = "e-shopping";
+
+string solutionPath = @"D:\api-backend";
+
+Console.WriteLine("The process is started.");
+
+FileCommand fileCommand = new FileCommand(currenstolutionName, newSolutionName, solutionPath);
+
+Console.WriteLine("The folder renamer operation is started.");
+fileCommand.ReplaceFolderName(solutionPath);
+Console.WriteLine("The Folders are renamed successfully.");
+
+Console.WriteLine("The file renamer operation is started.");
+fileCommand.ReplaceFileName(solutionPath);
+Console.WriteLine("The Files are renamed successfully.");
+
+Console.WriteLine("The content renamer operation is started.");
+fileCommand.ReplaceFileContent(solutionPath);
+Console.WriteLine("The File contents are renamed successfully.");
+
+Console.WriteLine("Process has completed successfully."); 
+Console.ReadLine();
